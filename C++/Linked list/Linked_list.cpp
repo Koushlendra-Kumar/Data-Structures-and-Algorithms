a6613fe675777032601db3aeec25c9cca99513cc
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-06-16 01:26:46 +0000

    :white_check_mark: Insert five nodes at the end

--- C++/Linked list/Linked_list.cpp
+++ C++/Linked list/Linked_list.cpp
@@ -6,17 +6,35 @@ struct Node {
 };
 
 struct Node* head = NULL;
-
+struct Node* tail = NULL;
 void InsertFiveNodesAtBegining()
 {
     for(int i = 0; i< 5; i++){
         int tempVal ;
         struct Node* tempNode = new Node;
-        std::cout << "Add node at the begining of the Linked List : ";
+        std::cout << "Add node at the BEGINING of the Linked List : ";
         std::cin >> tempVal;
         tempNode->val = tempVal;
         tempNode->next = head;
         head = tempNode;
+        if(tail == NULL){
+            tail = tempNode;
+        }
+    }
+}
+void InsertFiveNodesAtTheEnd()
+{
+    for(int i = 0; i< 5; i++){
+        int tempVal ;
+        struct Node* tempNode = new Node;
+        std::cout << "Add node at the END of the Linked List : ";
+        std::cin >> tempVal;
+        tempNode->val = tempVal;
+        tempNode->next = NULL;
+        if(tail->next == NULL){
+            tail->next = tempNode;
+        }
+        tail = tempNode;
     }
 }
 
@@ -33,6 +51,8 @@ void PrintList(Node* head){
 int main()
 {   
     InsertFiveNodesAtBegining();
+    PrintList(head);
+    InsertFiveNodesAtTheEnd();
     PrintList(head);
     return 0;
 }
